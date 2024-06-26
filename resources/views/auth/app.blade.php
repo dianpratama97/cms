a2f743f7006e3f38a934d9b1df68b44f265cbb37
dianpratama97 <dianpratama097@gmail.com>
2024-06-26 10:57:05 +0700

    update web

--- resources/views/auth/app.blade.php
+++ resources/views/auth/app.blade.php
@@ -12,42 +12,42 @@
 
     <meta name="description" content="" />
 
-   <!-- Favicon -->
-   <link rel="icon" type="image/x-icon" href="{{ asset('backend') }}/assets/img/favicon/logo.ico" />
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('backend') }}/assets/img/favicon/logo.ico" />
 
-   <!-- Fonts -->
-   <link rel="preconnect" href="https://fonts.googleapis.com" />
-   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-   <link
-     href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
-     rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
+        rel="stylesheet" />
 
-   <!-- Icons -->
-   <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/fonts/fontawesome.css" />
-   <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/fonts/tabler-icons.css" />
-   <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/fonts/flag-icons.css" />
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/fonts/fontawesome.css" />
+    <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/fonts/tabler-icons.css" />
+    <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/fonts/flag-icons.css" />
 
-   <!-- Core CSS -->
-   <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/css/rtl/core.css" />
-   <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/css/rtl/theme-default.css" />
-   <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/demo.css" />
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/css/rtl/core.css" />
+    <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/css/rtl/theme-default.css" />
+    <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/demo.css" />
 
-   <!-- Vendors CSS -->
-   <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/libs/node-waves/node-waves.css" />
-   <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-   <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/libs/typeahead-js/typeahead.css" />
-   <!-- Vendor -->
-   <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/libs/@form-validation/form-validation.css" />
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/libs/node-waves/node-waves.css" />
+    <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/libs/typeahead-js/typeahead.css" />
+    <!-- Vendor -->
+    <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/libs/@form-validation/form-validation.css" />
 
-   <!-- Page CSS -->
-   <!-- Page -->
-   <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/css/pages/page-auth.css" />
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="{{ asset('frontend') }}/assets/vendor/css/pages/page-auth.css" />
 
-   <!-- Helpers -->
-   <script src="{{ asset('frontend') }}/assets/vendor/js/helpers.js"></script>
-   <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
-   <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-   <script src="{{ asset('frontend') }}/assets/js/config.js"></script>
+    <!-- Helpers -->
+    <script src="{{ asset('frontend') }}/assets/vendor/js/helpers.js"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="{{ asset('frontend') }}/assets/js/config.js"></script>
 </head>
 
 <body>
@@ -67,29 +67,28 @@
 
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
- 
-    <script src="{{ asset('fronend') }}/assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="{{ asset('fronend') }}/assets/vendor/libs/popper/popper.js"></script>
-    <script src="{{ asset('fronend') }}/assets/vendor/js/bootstrap.js"></script>
-    <script src="{{ asset('fronend') }}/assets/vendor/libs/node-waves/node-waves.js"></script>
-    <script src="{{ asset('fronend') }}/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="{{ asset('fronend') }}/assets/vendor/libs/hammer/hammer.js"></script>
-    <script src="{{ asset('fronend') }}/assets/vendor/libs/i18n/i18n.js"></script>
-    <script src="{{ asset('fronend') }}/assets/vendor/libs/typeahead-js/typeahead.js"></script>
-    <script src="{{ asset('fronend') }}/assets/vendor/js/menu.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/libs/popper/popper.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/js/bootstrap.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/libs/node-waves/node-waves.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/libs/hammer/hammer.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/libs/i18n/i18n.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/libs/typeahead-js/typeahead.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/js/menu.js"></script>
 
     <!-- endbuild -->
 
     <!-- Vendors JS -->
-    <script src="{{ asset('fronend') }}/assets/vendor/libs/@form-validation/popular.js"></script>
-    <script src="{{ asset('fronend') }}/assets/vendor/libs/@form-validation/bootstrap5.js"></script>
-    <script src="{{ asset('fronend') }}/assets/vendor/libs/@form-validation/auto-focus.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/libs/@form-validation/popular.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/libs/@form-validation/bootstrap5.js"></script>
+    <script src="{{ asset('backend') }}/assets/vendor/libs/@form-validation/auto-focus.js"></script>
 
     <!-- Main JS -->
-    <script src="{{ asset('fronend') }}/assets/js/main.js"></script>
+    <script src="{{ asset('backend') }}/assets/js/main.js"></script>
 
     <!-- Page JS -->
-    <script src="{{ asset('fronend') }}/assets/js/pages-auth.js"></script>
+    <script src="{{ asset('backend') }}/assets/js/pages-auth.js"></script>
 </body>
 
 </html>
